ccc5dcba7334fc9cfbeb6225340b586aeb4b9d3d
Anushika2703 <youremail@example.com>
2026-09-15 13:28:38 +0530

    Fix attendance images state key

--- src/screens/teacher_screen.py
+++ src/screens/teacher_screen.py
@@ -107,11 +107,11 @@ def teacher_tab_take_attendance():
     selected_subject_id=subject_options[selected_subject_label]
 
     st.divider()
-    if st.session_state.attendence_images:
+    if st.session_state.attendance_images:
         st.header('Added Photos')
         gallery_cols=st.columns(4)
 
-        for idx, img in enumerate(st.session_state.attendence_images):
+        for idx, img in enumerate(st.session_state.attendance_images):
             with gallery_cols[idx%4]:
                 st.image(img, width='stretch',caption=f'Photo {idx+1}')
     has_photos=bool(st.session_state.attendance_images)    
